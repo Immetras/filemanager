707a88474f549c99e96bc582eaa422bca64ad3fd
immetras <eranusieq@gmail.com>
2024-01-09 10:39:54 +0100

    i just don't know

--- server.js
+++ server.js
@@ -13,43 +13,41 @@ const context = {
   root: [],
 };
 
+//*
+//// app.get("/", function (req, res) {
+////   res.render("upload.hbs", context);
+//// });
+//// app.post("/handleUpload", function (req, res) {
+////   let form = formidable({});
+////   form.keepExtensions = true;
+
+////   form.uploadDir = __dirname + "/static/upload/";
+
+////   form.parse(req, function (err, fields, files) {
+////     let wys = [];
+////     wys.push(fields);
+////     // let obj = {};
+////     // wys.push(obj);
+////     console.log(wys);
+
+////     res.header("content-type", "application/json");
+////     res.send(JSON.stringify(wys, null, 4));
+////   });
+//// });
+
+//// app.get("/filemanager", function (req, res) {
+////   res.render("filemanager.hbs");
+//// });
+
+//// app.get("/info", function (req, res) {
+////   res.render("info.hbs");
+//// });
+
 app.get("/", function (req, res) {
-  res.render("upload.hbs", context);
-});
-app.post("/handleUpload", function (req, res) {
-  let form = formidable({});
-  form.keepExtensions = true;
-
-  form.uploadDir = __dirname + "/static/upload/";
-
-  form.parse(req, function (err, fields, files) {
-    let wys = [];
-    wys.push(fields);
-    let obj = {
-      imageupload: {
-        size: files["image"]["size"],
-        path: files["image"]["path"],
-        name: files["image"]["name"],
-        type: files["image"]["type"],
-        mtime: files["image"]["lastModifiedDate"],
-      },
-    };
-    wys.push(obj);
-    console.log(wys);
-
-    res.header("content-type", "application/json");
-    res.send(JSON.stringify(wys, null, 4));
-  });
+  res.render();
 });
 
-app.get("/filemanager", function (req, res) {
-  res.render("filemanager.hbs");
-});
-
-app.get("/info", function (req, res) {
-  res.render("info.hbs");
-});
-
+//* hbs things
 app.set("views", path.join(__dirname, "static/views"));
 app.engine(
   "hbs",

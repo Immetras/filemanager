3a23737e2f9707a2cf6caa6108d15d88c35cf31e
immetras <eranusieq@gmail.com>
2024-01-22 12:39:47 +0100

    mainly css

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = 3000;
 const path = require("path");
 const hbs = require("express-handlebars");
-const Datastore = require("nedb");
-const { mainModule } = require("process");
-const formidable = require("formidable");
+const fs = require("fs");
+// const { mainModule } = require("process");
+// const formidable = require("formidable");
 
 app.use(express.static("static"));
 
@@ -14,37 +14,13 @@ const context = {
 };
 
 //*
-//// app.get("/", function (req, res) {
-////   res.render("upload.hbs", context);
-//// });
-//// app.post("/handleUpload", function (req, res) {
-////   let form = formidable({});
-////   form.keepExtensions = true;
-
-////   form.uploadDir = __dirname + "/static/upload/";
-
-////   form.parse(req, function (err, fields, files) {
-////     let wys = [];
-////     wys.push(fields);
-////     // let obj = {};
-////     // wys.push(obj);
-////     console.log(wys);
-
-////     res.header("content-type", "application/json");
-////     res.send(JSON.stringify(wys, null, 4));
-////   });
-//// });
-
-//// app.get("/filemanager", function (req, res) {
-////   res.render("filemanager.hbs");
-//// });
-
-//// app.get("/info", function (req, res) {
-////   res.render("info.hbs");
-//// });
 
 app.get("/", function (req, res) {
-  res.render();
+  res.redirect("/filemanager");
+});
+
+app.get("/filemanager", function (req, res) {
+  res.render("files.hbs");
 });
 
 //* hbs things
